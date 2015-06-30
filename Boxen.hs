105254f8069da1e46d0215bf1e6ab797111b9af5
Daniel Bergey <bergey@teallabs.org>
2015-06-30 21:33:53 +0000

    delete unused code

--- Boxen.hs
+++ Boxen.hs
@@ -12,14 +12,6 @@ import qualified Options.Applicative       as O
 import qualified Options.Applicative.Types as O
 import           Prelude                   hiding (length, reverse)
 
--- units are taken to be inches if this were built to full design scale
-
--- thickness, shelfHeight, shelfLength, depth :: Double
--- thickness = 0.125*12 -- plywood thickness
--- shelfHeight = 14 -- shelf height
--- shelfLength = 36
--- depth = 12
-
 main :: IO ()
 main = do
   config <- O.execParser helpParser
@@ -98,13 +90,6 @@ boxenPieces config = vcat [ side
                 , notchTrail (nLength ns) _xDir, notchTrail (nWidth ns) _yDir ]
   top = centerXY $ rect (length config) (width config)
 
--- boxenDrawn :: Diagram SVG R2
--- boxenDrawn = vcat [ side config # alignR
---                   , strutY 1
---                   , middleRow # alignR
---                   ] # lw 0.05 # centerXY where
---   middleRow = hcat [end config, strutX 1, base config]
-
 notch :: (Vn t ~ V2 Double, TrailLike t) => Config -> t
 notch config = fromOffsets [ -l *^ unitY
                         , t *^ unitX
@@ -119,29 +104,6 @@ notch config = fromOffsets [ -l *^ unitY
 -- handle = either (error.show) id $
 --          MF.fromString --"(-3,0){1,0}..{1,0}(3,0)..(3,0.75)..(0,2)..(-3,0.75)..cycle"
 --          "(0,0)..{1,0}(3,0)..(3,0.75)..(0,2)..(-3,0.75)..(-3,0){1,0}..cycle"
-
--- | notchesDividing d n v places n notches of depth d along v so that
--- they evenly divide the length.
-
--- notchesDividing ::  (V t ~ R2, TrailLike t, Transformable t) =>
---                     Double -> Int -> R2 -> t
--- notchesDividing d n v = rotate a . trailLike . flip at origin . mconcat . replicate n $ unit where
---   unit = notch d (magnitude v / realToFrac n / 2) :: Trail R2
---   a = direction v :: Turn
-
--- | notchesLessThan d l v places n notches of depth d along v so that
--- they evenly divide the length, with n chosen as the largest integer
--- such that |v|/n < d
-
--- notchesLessThan ::  (V t ~ R2, TrailLike t, Transformable t) =>
---                     Double -> Double -> R2 -> t
--- notchesLessThan d l v = notchesDividing d n v where
---   n = ceiling $ magnitude v / l / 2
-
--- notchesMoreThan ::  (V t ~ R2, TrailLike t, Transformable t) =>
---                     Double -> Double -> R2 -> t
--- notchesMoreThan d l v = notchesDividing d n v where
---   n = floor $ magnitude v / l / 2
 
 -- Many heuristics are possible to pick a notch length.  I think boxes
 -- look better if all the notches along one side are the same length,
@@ -187,18 +149,6 @@ notchPair joint dir = fromOffsets
 -- assembled box.
 notchTrail :: (TrailLike t, Vn t ~ V2 Double, Monoid t) => Joint -> Direction V2 Double -> t
 notchTrail joint dir = mconcat $ replicate (count joint) (notchPair joint dir)
-
--- lengthNotches, heightNotches, depthNotches :: Trail R2
--- lengthNotches = notchesMoreThan thickness 3 $ (shelfLength - 2 * thickness) *^ unitX
-
--- heightNotches = notchesMoreThan thickness 3 $ unitY ^* (shelfHeight - 2*thickness)
-
--- depthNotches = notchesMoreThan thickness 3 $ unitX ^* (depth - thickness)
-
--- lengthStraight, heightStraight :: Trail R2
--- lengthStraight = fromOffsets [unit_X ^* (shelfLength - 2*thickness)]
-
--- heightStraight = fromOffsets [unit_Y ^* (shelfHeight - 2*thickness)]
 
 reverse :: (Transformable t, V t ~ V2, Floating (N t)) => t -> t
 reverse = rotateBy 0.5
