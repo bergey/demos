e1c863be1e53ba82c0f7708508bf4bdb7b9ef619
Daniel Bergey <bergey@teallabs.org>
2015-07-08 17:29:21 +0000

    spheres1: update for CSG

--- spheres1.hs
+++ spheres1.hs
@@ -15,7 +15,7 @@ color c = diffuse 0.5 . ambient 0.1 . sc c
 
 example :: Diagram POVRay
 example = centerX $ cat unitX
-  [sphere # color cyan, sphere # color green, sphere # color blue]
+  [skin sphere # color cyan, skin sphere # color green, skin sphere # color blue]
 
 main :: IO ()
 main = putStrLn $ renderDia POVRay POVRayOptions $ mconcat [example, cam, light]
